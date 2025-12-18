0000da4e8bd68ff7c3ba3d22c004badfd44b32f9
DreDabe <2331066631@qq.com>
2025-12-18 17:32:05 +0800

    添加图片文件类型声明，解决TypeScript编译错误

---
+++ declarations.d.ts
@@ -0,0 +1,25 @@
+// 为图片文件添加类型声明，解决TypeScript编译错误
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
